3f5c1adb1066167e7e1ff91b46c714d26b190dae
PxlBuzzard <daniel@danieljost.com>
2020-06-09 20:39:46 -0700

    Added a player component.

---
+++ src/Player.ts
@@ -0,0 +1,52 @@
+import {
+    useType,
+    useNewComponent,
+    Image,
+    Keyboard,
+    useDraw,
+    useUpdate,
+} from "@hex-engine/2d";
+
+export default function Player(): void {
+    useType(Player);
+    let posX = 0;
+    let posY = 0;
+
+    const player = useNewComponent(() =>
+        Image({
+            url: "./assets/idle_1.png"
+        })
+    );
+
+    const keyboard = useNewComponent(() =>
+        Keyboard()
+    );
+
+    //useNewComponent(() => Physics.Body(player));
+
+    useUpdate(() => {
+        keyboard.pressed.forEach(key => {
+            switch (key) {
+            case "ArrowDown":
+                posY += 5;
+                break;
+            case "ArrowUp":
+                posY -= 5;
+                break;
+            case "ArrowLeft":
+                posX -= 5;
+                break;
+            case "ArrowRight":
+                posX += 5;
+                break;
+            }
+        });
+    });
+
+    useDraw((context) => {
+        player.draw(context, {
+            x: posX,
+            y: posY
+        });
+    });
+}
